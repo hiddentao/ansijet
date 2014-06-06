41a285b2bbdf064bdf8bc0ab56de0041e71a8758
Ramesh Nair <ram@hiddentao.com>
2014-06-06 16:19:29 +0800

    Initial styles

---
+++ gulpfile.js
@@ -0,0 +1,58 @@
+var gulp = require('gulp'),
+  path = require('path');
+
+// require('gulp-grunt')(gulp); // add all the gruntfile tasks to gulp
+
+var clean = require('gulp-clean');
+var nib = require('nib');
+var stylus = require('gulp-stylus');
+var minifyCSS = require('gulp-minify-css');
+
+
+
+var paths = {
+  buildFolder: './frontend/build',
+  cssBuildFolder: './frontend/build/css',
+  cssSrcFiles: './frontend/src/stylus/style.styl',
+};
+
+
+var logErrors = function() {
+  console.log(arguments);
+}
+
+
+gulp.task('clean', function() {
+  return gulp.src([ paths.buildFolder], {read: false})
+    .pipe(clean());
+});
+
+
+gulp.task('css', function () {
+  return gulp.src( paths.cssSrcFiles )
+    .pipe( stylus({
+      use: [ nib() ],
+      errors: true
+    }) )
+    // .pipe( minifyCSS({
+    //   keepSpecialComments: 0,
+    //   noAdvanced: true
+    // }) )
+    .pipe(gulp.dest( paths.cssBuildFolder ))
+    ;
+});
+
+
+
+// Rerun the task when a file changes
+gulp.task('watch-assets', ['css'], function() {
+  gulp.watch(paths.cssSrcFiles, ['css']); // watch the same files in our scripts task
+});
+
+
+
+// The default task (called when you run `gulp` from cli)
+gulp.task('default', ['clean', 'css']);
+
+
+
