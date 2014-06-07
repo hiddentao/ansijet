6e4e6e2bc5819de57203fbaee8438a7ba582dbeb
Ramesh Nair <ram@hiddentao.com>
2014-06-07 13:37:49 +0800

    Work towards Js setup

--- gulpfile.js
+++ gulpfile.js
@@ -4,9 +4,12 @@ var gulp = require('gulp'),
 // require('gulp-grunt')(gulp); // add all the gruntfile tasks to gulp
 
 var clean = require('gulp-clean');
+var concat = require('gulp-concat');
 var nib = require('nib');
 var stylus = require('gulp-stylus');
 var minifyCSS = require('gulp-minify-css');
+var uglify = require('gulp-uglify');
+var gzip = require('gulp-gzip');
 
 
 
@@ -15,6 +18,9 @@ var paths = {
   cssBuildFolder: './frontend/build/css',
   cssSrcFiles: './frontend/src/stylus/style.styl',
   cssSrcFilesWatch: './frontend/src/stylus/*.styl',
+
+  jsSrcFiles: './frontend/src/js/**/*.js',
+  jsBuildFolder: './frontend/build/js',
 };
 
 
@@ -35,25 +41,40 @@ gulp.task('css', function () {
       use: [ nib() ],
       errors: true
     }) )
-    // .pipe( minifyCSS({
-    //   keepSpecialComments: 0,
-    //   noAdvanced: true
-    // }) )
-    .pipe(gulp.dest( paths.cssBuildFolder ))
+    .pipe( minifyCSS({
+      keepSpecialComments: 0,
+      noAdvanced: true
+    }) )
+    .pipe( gulp.dest( paths.cssBuildFolder ) )
+    .pipe( gzip({ gzipOptions: { level: 9 } }) ) 
+    .pipe( gulp.dest(paths.jsBuildFolder) )
     ;
 });
 
 
 
+gulp.task('js', function() {
+  return gulp.src( paths.jsSrcFiles )
+    .pipe( concat() )
+    .pipe( uglify() )
+    .pipe( gulp.dest(paths.jsBuildFolder) )
+    .pipe( gzip({ gzipOptions: { level: 9 } }) ) 
+    .pipe( gulp.dest(paths.jsBuildFolder) )
+    ;
+})
+
+
+
 // Rerun the task when a file changes
-gulp.task('watch', ['css'], function() {
+gulp.task('watch', ['css', 'js'], function() {
   gulp.watch(paths.cssSrcFilesWatch, ['css']); // watch the same files in our scripts task
+  gulp.watch(paths.jsSrcFiles, ['js']); // watch the same files in our scripts task
 });
 
 
 
 // The default task (called when you run `gulp` from cli)
-gulp.task('default', ['clean', 'css']);
+gulp.task('default', ['clean', 'css', 'js']);
 
 
 
